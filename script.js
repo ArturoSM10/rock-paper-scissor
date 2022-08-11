a21365f0f2fe53347173f439e87f4ac193a9a67a
Arturo <jarsmdza@gmail.com>
2022-08-10 19:37:20 -0500

    Added function as asked in the exercise and also one extra for playing again

--- script.js
+++ script.js
@@ -1,26 +1,77 @@
-let computerChoice = (Math.floor(((Math.random())*3))+1);
-let userChoice = prompt(`Select one`);
-let r = getUserChoice(userChoice);
-console.log(`tu resultado fue ${r} y el de la computadora fue ${computerChoice}`);
-let  inputLower= userChoice.toLowerCase();
+let playerScore = 0,
+    computerScore = 0,
+    computerSelection = [`rock`,`paper`,`scissors`],
+    currentGame,
+    newGame = 0;
 
-//Create function getComputerChoice
+while (newGame === 0) {
+    currentGame = game();
+    console.log(currentGame);
+    //Aditional delete later when interface is finished
+    newGame = playAgain();
+}
 
-//Create function getUserChoice 
-function getUserChoice(input="no valid option") {
-    let  inputLower= input.toLowerCase();
-    let result;
-    if (inputLower==="rock") {
-        result = 1;
-    }
-    else if (inputLower==="paper"){
-        result = 2;
-    }
-    else if (inputLower==="scissor") {
-        result= 3;
+//function playAgain 
+function playAgain() {
+    newGame = prompt(`¿Do you want to play again? (Y/N)`).toLowerCase();
+    if (newGame === `y`) {
+        playerScore = 0;
+        computerScore = 0;
+        return 0;
     }
     else {
-        result = "no valid option";
+        return 1;
     }
-    return result;
 }
+
+//function getComputerChoice
+function getComputerChoice() {
+    let i = (Math.floor(Math.random()*3));
+    return computerSelection[i];
+}
+
+//function playerSelection 
+function playerSelection() {
+    const userChoice = prompt(`Select one`).toLocaleLowerCase();
+    return userChoice;
+}
+
+//function playRound 
+function playRound(player, computer) {
+    if (player === computer) {
+        return `You got ${player} and computer got ${computer} so, no one wins`;
+    }
+    else if ((player === `rock` && computer === `scissors`) || 
+            (player === `paper` && computer === `rock`) ||
+            (player === `scissors` && computer === `paper`)) {
+                return `You win ${player} beats ${computer}`;
+            }
+    else {
+        return `You lose ${player} is beaten by ${computer}`;
+    }
+}
+
+//function game 
+function game(){
+    let result;
+    for (let i = 0; i<5; i++) {
+        result = playRound(playerSelection(),getComputerChoice());
+        console.log(result); //delete after the interface is finished
+        if (result.includes(`You win`)) {
+            playerScore += 1;
+        }
+        if (result.includes(`You lose`)) {
+            computerScore += 1;
+        }
+    }
+        if (playerScore > computerScore) {
+            result = `You win ${playerScore} vs ${computerScore}`;
+        }
+        else if (playerScore < computerScore) {
+            result = `You lose ${playerScore} vs ${computerScore}`;
+        }
+        else {
+            result = `No one wins ${playerScore} vs ${computerScore}`;
+        }
+    return result;
+    }
